c17efb9a9c21b0b7f26688f8675820b03773fb77
jso0 <i@Ijimmy.tk>
2017-03-01 19:58:51 +0800

    support webpack2 /lib_map mini

--- index.js
+++ index.js
@@ -9,29 +9,85 @@ var defaultOpt = {
 	show:false
 }
 
+var libMap = {
+	'jquery-cookie': {
+		'pfx': 'jquery-cookie',
+		'type': 'js',
+		'file': 'jquery.cookie',
+		'defaultVersion': '1.4.1',
+		'exports': '',
+		'show': false
+	},
+	'$': {
+		'pfx': 'jquery',
+		'type': 'js',
+		'file': 'jquery',
+		'defaultVersion': '3.1.1',
+		'exports': 'jQuery',
+		'show': true
+	},
+	'vue': {
+		'pfx': 'vue',
+		'type': 'js',
+		'file': 'vue',
+		'defaultVersion': '2.1.10',
+		'exports': 'Vue',
+		'show': true
+	},
+	'normalize': {
+		'pfx': 'normalize',
+		'type': 'css',
+		'file': 'normalize',
+		'defaultVersion': '5.0.0',
+		'exports': '',
+		'show': false
+	},
+	'animate.css': {
+		'pfx': 'animate.css',
+		'type': 'css',
+		'file': 'animate',
+		'defaultVersion': '3.5.2',
+		'exports': '',
+		'show': false
+	},
+	'loader_error': {
+		'pfx': '',
+		'type': 'js',
+		'file': '',
+		'defaultVersion': '',
+		'exports': '',
+		'show': false
+	}
+}
+
+
 module.exports = function(source) {
 	var query = loaderUtils.parseQuery(this.query);
-	if (this.query && this.query.indexOf('@') > 0) {
-		var css = !!query.css
-		var regRule = /^\?\w+@[\w\.\,]+$/
+	if (this.query) {
+		var regRule = /^\?.*@?$/
 		var tmp = this.query.split(/&|\,/)
 		var lib = tmp[0].replace(/\!|\?/, '').split('@')
+		var currentLib = libMap[lib[0]] || libMap['loader_error']
+		var is_css =  currentLib.type ==  'css' || !!query.css
 
 		if (regRule.test(tmp[0])) {
 			var cdn = extend({}, defaultOpt, {
-				"name": lib[0],
-				"type": css ? 'css' : 'js',
-				"version": lib[1]
+				"name": currentLib.pfx,
+				"exports": currentLib.exports,
+				"type": is_css ? 'css' : 'js',
+				"version": lib[1] || currentLib.defaultVersion,
+				"show": currentLib.show,
+				"file": query.file || currentLib.file
 			}, query)
 			var link_args = JSON.stringify(cdn)
-			var windows = "module.exports = window."+ cdn.name +";"
-			if (cdn.type == "css") {
+			var windows = "module.exports = window."+ cdn.exports +";"
+			if (is_css) {
 				windows = ''
 			}
 			if (cdn.show) {
 				return "( " + link_args + ")//\n" + windows
 			} else {
-				return "/*" + link_args + "*/\n" + windows
+				return "/*" + link_args + "*/\n"
 			}
 		}
 		return '/* parameter invalid [name@version] */'
